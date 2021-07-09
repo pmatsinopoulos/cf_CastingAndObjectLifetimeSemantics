25eb8c06a934c35667c734bc364f0e7ce11d23a1
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-09 08:16:40 +0300

    Initial Commit

--- CastingAndObjectLifetimeSemantics/main.m
+++ CastingAndObjectLifetimeSemantics/main.m
@@ -6,11 +6,21 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreFoundation/CoreFoundation.h>
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+    NSLocale *pGbNSLocale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_GB"];
+    CFLocaleRef rfGbCFLocale = (__bridge CFLocaleRef)pGbNSLocale; // transfers a pointer from Objective-C to CF without transfer of ownership
+    
+    CFStringRef cfIdentifier = CFLocaleGetIdentifier(rfGbCFLocale);
+    NSLog(@"cfIdentifier: %@", (__bridge NSString *)cfIdentifier);
+    
+    CFLocaleRef myLocaleRef = CFLocaleCopyCurrent();
+    NSLocale *myNSLocale = (NSLocale *)CFBridgingRelease(myLocaleRef);
+    
+    NSString *pnsIdentifier = [myNSLocale localeIdentifier];
+    CFShow((CFStringRef)[@"nsIdentifier: " stringByAppendingString:pnsIdentifier]);
   }
   return 0;
 }
